8a2060362e82bb382ba369ae78545c354f1a63e4
atroopa <atroopa@gmail.com>
2023-04-21 18:13:49 +0330

    delete button work success

--- src/components/ProductLists.jsx
+++ src/components/ProductLists.jsx
@@ -7,53 +7,51 @@ class ProductLists extends Component {
   render() {
     return (
         <div className="relative overflow-x-auto">
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" className="px-6 py-3">
-                    ID
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    TITLE
-                </th>
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            TITLE
+                        </th>
 
-                <th scope="col" className="px-6 py-3">
-                    Price
-                </th>
-            </tr>
-        </thead>
-        <tbody>
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
 
-            {this.props.products.map(item => 
-                <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {item.id}
-                    </th>
-                    <td className="px-6 py-4">
-                        {item.productName}
-                    </td>
-                    <td className="px-6 py-4">
-                        {"$"+item.price}
-                    </td>
-                    <td>
-                        <button type="button" className="text-white py-2 px-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...">
-                            delete
-                        </button>
-                    </td>
-
-                </tr>
-            )}
-        </tbody>
-    </table>
-</div>
+                {this.props.products.map(item => 
+                    <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {item.id}
+                        </th>
+                        <td className="px-6 py-4">
+                            {item.productName}
+                        </td>
+                        <td className="px-6 py-4">
+                            {"$"+item.price}
+                        </td>
+                        <td>
+                            <button onClick={() => this.props.removeProduct(item.id)} type="button" className="text-white py-2 px-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...">
+                                delete
+                            </button>
+                        </td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    </div>
     )
   }
 }
 
 
-function mapStateToProps(state) {
-    return{
-        
+const mapStateToProps = (state)  => {
+    return{   
         products : state.productState.items
     }
 }
@@ -64,4 +62,4 @@ const mapDispathToProps = dispath => {
     }
 }
 
-export default connect(mapStateToProps, {getAll})(ProductLists);
+export default connect(mapStateToProps, mapDispathToProps )(ProductLists);
