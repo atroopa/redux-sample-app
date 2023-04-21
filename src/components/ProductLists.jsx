db91dac4ab9b11316c49e1aac024553d0a2187af
atroopa <atroopa@gmail.com>
2023-04-21 11:43:01 +0330

    create ProductLists Component

---
+++ src/components/ProductLists.jsx
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+
+
+export default class ProductLists extends Component {
+  render() {
+    return (
+        <div>
+            ProductLists
+        </div>
+    )
+  }
+}
