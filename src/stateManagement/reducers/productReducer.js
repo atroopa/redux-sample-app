790092368fa29a81a4762441eb5dda51be597af4
atroopa <atroopa@gmail.com>
2023-04-21 10:46:07 +0330

    reducers ready to use

--- src/stateManagement/reducers/productReducer.js
+++ src/stateManagement/reducers/productReducer.js
@@ -4,11 +4,15 @@ import {
     ProductADD,
     ProductRemove,
     ProductEdit
-} from './actionTypes';
+} from '../actions/actionTypes';
 
 
 const initialState = {
-    items:     [],
+    items:     [
+        {id:1 , productName: "Product A", price: 100},
+        {id:2 , productName: "Product B", price: 200},
+        {id:3 , productName: "Product C", price: 300}
+    ],
     isLoading: true,
 };
 
@@ -18,11 +22,21 @@ function ProductReducer(state, action) {
     switch (action.type) {
 
         case ProductGetAll:
-            
-            break;
+            return {...state, items:state.items};
+
+        case ProductGetById:
+            return {...state, items:state.items.find(q => q.id === action.payload)};
     
+        case ProductADD:
+            return {...state , items: state.items.concat([action.payload])};
+
+        case ProductRemove:
+            return {...state, items:state.item.filter(q => q.id != action.payload)};    
+
         default:
-            break;
+            return state;
     }
 
 }
+
+export default ProductReducer;
