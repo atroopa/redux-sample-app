9a7e86480d8f66dfd644c5c630411183b62364d2
atroopa <atroopa@gmail.com>
2023-04-21 09:58:41 +0330

    make all action creators

---
+++ src/stateManagement/actions/productAction.js
@@ -0,0 +1,26 @@
+// GetAll
+// GetById
+// Add
+// Remove
+// Edite
+
+export const getAll = () => ({
+    type:"PRODUCT_GET_ALL",
+
+});
+
+
+export const getByiD = id => ({
+    type: "PRODUCT_GET_BY_ID",
+    pyload: id
+});
+
+export const add = item => ({
+    type: "PRODUCT_ADD",
+    payload: item
+});
+
+export const remove = id => ({
+    type: "PRODUCT_REMOVE",
+    payload: id
+});
