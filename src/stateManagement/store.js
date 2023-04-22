067464b4e083c4769c8d444fdb88898b2300859b
atroopa <atroopa@gmail.com>
2023-04-22 13:03:13 +0330

    install redux devtools and us that code

--- src/stateManagement/store.js
+++ src/stateManagement/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import logger from 'redux-logger'
 
 
 const persistConfig = {
@@ -16,5 +17,8 @@ const persistedReducer = persistReducer(persistConfig, reducers)
 
 // export default store;
 
-export const store = createStore(persistedReducer)
+//export const store = createStore(persistedReducer, applyMiddleware(logger))
+export const store = createStore(persistedReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 export const persistor = persistStore(store)
